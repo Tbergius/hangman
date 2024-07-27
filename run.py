3cc197fab5c7d0934324ab09954e2e00fc479ba7
Thomas van den Berg <toegangtot@gmail.com>
2024-07-27 13:48:16 +0000

    Initial commit with basic game structure

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def load_words():
+    """
+    Loads a list of words for the Hangman game.
+    """
+    return ["python", "hangman", "challenge", "programming", "developer"]
+
+
+def main():
+    """
+    Main function to start the Hangman game.
+    """
+    words = load_words()
+    print("Welcome to Hangman!")
+
+
+if __name__ == "__main__":
+    main()
