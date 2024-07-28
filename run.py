15327a6379612833143ba9e065b38e5f809fc85d
Thomas van den Berg <toegangtot@gmail.com>
2024-07-28 13:07:01 +0000

    Added user input to guess letters and validate for letters, not numbers

--- run.py
+++ run.py
@@ -16,7 +16,19 @@ def display_word(word, guessed_letters):
     display = [letter if letter in guessed_letters else '_' for letter in word]
     print (' '.join(display))
 
-
+def get_user_input(guessed_letters):
+    """
+    Promts player to guess a letter, validating for letter.
+    Args:
+        guessed_letters (set): The set of letters that have been guessed.
+    Returns:
+        str: A valid guessed letter.
+    """
+    while True:
+        guess = input("Guess a letter: ").lower()
+        if len(guess) == 1 and guess.isalpha() and guess not in guessed_letters:
+            return guess
+        print("Invalid input. Please enter a single letter that you did not guess before.")
 
 def main():
     """
@@ -30,6 +42,11 @@ def main():
     print("Welcome to Hangman!")
     print(f"The word has {len(word)} letters.")
     display_word(word,guessed_letters)
+
+    while True:
+        guess = get_user_input(guessed_letters)
+        guessed_letters.add(guess)
+        display_word(word, guessed_letters)
 
 
 if __name__ == "__main__":
